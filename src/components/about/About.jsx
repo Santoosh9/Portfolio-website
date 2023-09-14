d5b0090693967663ba0ac43dd415672f1e0024e7
Santoosh9 <santoshgajurel17@gmail.com>
2023-09-14 21:57:10 +0545

    test

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <div className=" w-full h-[800px] pb-20 flex border-b-[1px] border-b-black">
-      About
+      Abou
     </div>
   );
 };
