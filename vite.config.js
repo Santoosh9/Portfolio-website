900d81ef13e0f5423c1527dca72db46ac5c49c75
Santoosh9 <santoshgajurel17@gmail.com>
2023-09-23 19:02:21 +0545

    update on vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: 'https://github.com/Santoosh9/Portfolio-website.git',
+
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 });
